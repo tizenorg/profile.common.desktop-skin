e17967a3e09f89ec7621ec8f50b8cce41f1cff85
Stephane Desneux <stephane.desneux@open.eurogiciel.org>
2015-02-27 18:31:20 +0100

    video scripts: replace playbin by custom pipeline
    playbin doesn't work well in some situations. Typically when
    running on a full HD screen, the video is not scaled to full screen. Also, sometimes,
    videos do not play at all, depending on hardware (fails to run on NUC E3815)
    
    Change-Id: Ic8f3f2ad725041dd7559cfdb6098cdc0625313ed
    Signed-off-by: Stephane Desneux <stephane.desneux@open.eurogiciel.org>

--- Script/launch_video.sh
+++ Script/launch_video.sh
@@ -4,18 +4,23 @@
 #  Launch web cam 
 #
 
-touch $HOME/gstvid-pid
-alreadyrunning=`cat $HOME/gstvid-pid`
+pidfile=$HOME/.gstvid-pid
 
-URL=file:///usr/share/media/videos/AmazingNature_480p.mp4
+touch $pidfile
+alreadyrunning=`cat $pidfile`
+
+video=/usr/share/media/videos/AmazingNature_480p.mp4
 
 if [ $(echo $alreadyrunning | wc -w) -lt 3  ] ; then
     echo "create new"
-    gst-launch-1.0 playbin uri=$URL &
-    echo $!  >> $HOME/gstvid-pid
+    #gst-launch-1.0 playbin uri=$URL &
+	gst-launch-1.0  filesrc location=$video ! qtdemux name=demux  \
+		demux.audio_0 ! queue ! decodebin ! audioconvert ! audioresample ! autoaudiosink  \
+		demux.video_0 ! queue ! vaapidecode ! vaapisink &
+    echo $!  >> $pidfile
 else echo "already running"
     for x in $alreadyrunning; do kill -9 $x; done
-    rm -rf $HOME/gstvid-pid
+    rm -rf $pidfile
 fi
 
 
