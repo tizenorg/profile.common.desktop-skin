7c59e1de7c9ea52e468875bba1e7d612b1e4376d
Stephane Desneux <stephane.desneux@open.eurogiciel.org>
2015-03-02 14:07:07 +0100

    Fix video playing when VA-API is not available
    Change-Id: Ic0b318da3d14041a64209a4f176b174973a13e6c
    Signed-off-by: Stephane Desneux <stephane.desneux@open.eurogiciel.org>

--- Script/launch_video.sh
+++ Script/launch_video.sh
@@ -13,10 +13,14 @@ video=/usr/share/media/videos/AmazingNature_480p.mp4
 
 if [ $(echo $alreadyrunning | wc -w) -lt 3  ] ; then
     echo "create new"
-    #gst-launch-1.0 playbin uri=$URL &
-	gst-launch-1.0  filesrc location=$video ! qtdemux name=demux  \
-		demux.audio_0 ! queue ! decodebin ! audioconvert ! audioresample ! autoaudiosink  \
-		demux.video_0 ! queue ! vaapidecode ! vaapisink &
+	if gst-inspect-1.0  | grep ^vaapi &>/dev/null; then
+		gst-launch-1.0  filesrc location=$video ! qtdemux name=demux  \
+			demux.audio_0 ! queue ! decodebin ! audioconvert ! audioresample ! autoaudiosink  \
+			demux.video_0 ! queue ! vaapidecode ! vaapisink &
+	else
+		echo "Playing without VA-API"
+		gst-launch-1.0 playbin uri=file://$video video-sink=waylandsink &
+	fi
     echo $!  >> $pidfile
 else echo "already running"
     for x in $alreadyrunning; do kill -9 $x; done
